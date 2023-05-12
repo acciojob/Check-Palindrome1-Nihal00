149677b549211f834b76c4d340418eae2564a212
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-05-12 14:41:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,5 +2,17 @@
 
 function palindrome(str){
 
+	let strJoin = str.split('').join('');
+
+	let len = Math.floor(strJoin.length/2);
+
+	for(let i = 0; i < len; i++){
+		if(strJoin[i] !== strJoin[len-1-i]){
+			return false;
+		}
+	}
+
+	return true;
+	
 }
 module.exports = palindrome
